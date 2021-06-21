96ff6560d96eee42b9e918e69f8a7db2e18f6302
Alexsashka43 <63096544+Alexsashka43@users.noreply.github.com>
2021-06-21 18:21:37 +0300

    HW

---
+++ app/templatetags/filters_and_tags.py
@@ -0,0 +1,45 @@
+from django.template import library
+
+register = library.Library()
+
+
+@register.filter
+def get_item(some_dict: dict, key: str):
+    return some_dict.get(key)
+
+
+@register.filter
+def from_str_to_float(string: str):
+    try:
+        if string != '':
+            new_value = float(string)
+            return new_value
+        else:
+            return '-'
+    except ValueError as e:
+        return 'ValueError'
+
+
+@register.filter
+def change_color(value):
+    if type(value) == float:
+        if value < 0:
+            return '#0f7004'
+        elif (value >= 1) and (value < 2):
+            return '#fbc8c9'
+        elif (value >= 2) and (value < 5):
+            return '#f6767a'
+        elif value >= 5:
+            return '#fb0008'
+        else:
+            return 'white'
+    else:
+        return 'white'
+
+
+@register.filter
+def not_equal(value):
+    if value != 'Суммарная':
+        return True
+
+    return False
---
+++ app/views.py
@@ -0,0 +1,22 @@
+import csv
+
+from django.shortcuts import render
+
+
+def inflation_view(request):
+    template_name = 'inflation.html'
+    data = []
+    # чтение csv-файла и заполнение контекста
+    with open('inflation_russia.csv', 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        for row in reader:
+            data.append(row)
+        names_of_column = data[0].keys()
+
+    context = {
+        'data': data,
+        'name_of_column': names_of_column
+    }
+
+    return render(request, template_name,
+                  context)
